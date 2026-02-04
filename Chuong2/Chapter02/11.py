b3f8c9f52e0f9a9ddbdd342cacdc51ca3bb61cd4
quyduc-27 <quyduck27@gmail.com>
2026-02-04 22:54:20 +0700

    Refactor grading logic and input prompt

--- Chuong2/Chapter02/11.py
+++ Chuong2/Chapter02/11.py
@@ -1,9 +1,10 @@
-a = float(input('nhap diem tb cua ban: '))
+a = float(input('dtb cua b: '))
 if a >= 8:
     print('xep loai gioi')
 elif a >= 6.5 and a <= 7.9:
     print('xep loai kha')
 elif a >= 5.0 and a <= 6.4:
-    print('xep loai trung binh')
+    print('xep loai tb')
 else:
+
     print ('xep loai yeu')
